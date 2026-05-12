e5a26c67205b9ef8b0b37c8e2ca4bc21a26f9e54
manningcameron-lang <manning.cameron@gmail.com>
2026-05-12 16:15:12 +0200

    Add 'Studio' link to navigation

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,6 +8,7 @@ const nav = [
   { href: "/", label: "Home" },
   { href: "/about", label: "About" },
   { href: "/services", label: "Services" },
+  { href: "/studio", label: "Studio" },
   { href: "/faq", label: "FAQ" },
   { href: "/contact", label: "Contact" },
 ];
